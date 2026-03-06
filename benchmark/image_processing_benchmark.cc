b180ec97d653cd578a06b8846b04071ec71b6f92
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-06 14:32:48 -0800

    Refactored benchmarks to dynamically register tests for new Python scripts

--- benchmark/image_processing_benchmark.cc
+++ benchmark/image_processing_benchmark.cc
@@ -7,81 +7,55 @@
 
 // global image directory path
 static const std::filesystem::path imageDirectory = PROJECT_ROOT_DIR "/images";
+static cv::Mat image;
 
 // processors only ever created once
 static std::unique_ptr<CppImageProcessor> cppImageProcessor = std::make_unique<CppImageProcessor>();
 static std::unique_ptr<PyImageProcessor> pyImageProcessor = std::make_unique<PyImageProcessor>();
 
-namespace py = pybind11;
-
-class CppImageProcessorFixture : public ::benchmark::Fixture
-{
-protected:
-    cv::Mat image;
-
-    void SetUp(::benchmark::State &state) override
-    {
-        try
-        {
-            std::filesystem::path imagePath = imageDirectory / "two_vertical_and_horizontal.tiff";
-
-            image = cv::imread(imagePath, cv::ImreadModes::IMREAD_GRAYSCALE);
-
-            if (image.empty())
-            {
-                state.SkipWithError("Failed to read from " + imagePath.string());
-                return;
-            }
-        }
-        catch (const std::filesystem::filesystem_error &e)
-        {
-            state.SkipWithError(e.what());
-            return;
-        }
-    }
-};
-
-BENCHMARK_F(CppImageProcessorFixture, CppImageProcessorBenchmark)(::benchmark::State &state)
+auto peformanceBenchmark = [](::benchmark::State &state, const ImageProcessor * processor, const cv::Mat &image, const std::string method)
 {
     for (auto _ : state)
     {
-        cppImageProcessor->execute(image, "fmm");
-    }
-}
-
-class PyImageProcessorFixture : public ::benchmark::Fixture
-{
-protected:
-    cv::Mat image;
-
-    void SetUp(::benchmark::State &state) override
-    {
-        try
-        {
-            std::filesystem::path imagePath = imageDirectory / "two_vertical_and_horizontal.tiff";
-
-            image = cv::imread(imagePath, cv::ImreadModes::IMREAD_GRAYSCALE);
-
-            if (image.empty())
-            {
-                state.SkipWithError("Failed to read from " + imagePath.string());
-                return;
-            }
-        }
-        catch (const std::filesystem::filesystem_error &e)
-        {
-            state.SkipWithError(e.what());
-            return;
-        }
+        processor->execute(image, method);
     }
 };
 
-BENCHMARK_F(PyImageProcessorFixture, PyImageProcessorBenchmark)(::benchmark::State &state)
+int main(int argc, char *argv[])
 {
-    for (auto _ : state)
+    // set up the static image for processing
+    cv::Mat image;
+    try
     {
-        pyImageProcessor->execute(image, "fmm");
-    }
-}
+        std::filesystem::path imagePath = imageDirectory / "two_vertical_and_horizontal.tiff";
 
-BENCHMARK_MAIN();
+        image = cv::imread(imagePath, cv::ImreadModes::IMREAD_GRAYSCALE);
+
+        if (image.empty())
+        {
+            std::cerr << "Failed to read from " << imagePath.string() << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+    catch (const std::filesystem::filesystem_error &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // register single cpp method for processing
+    ::benchmark::RegisterBenchmark(cppImageProcessor->getName(""), peformanceBenchmark, cppImageProcessor.get(), image, "fmm");
+
+    // register all python methods for processing
+    for (const auto &method : pyImageProcessor->getMethods())
+    {
+        ::benchmark::RegisterBenchmark(pyImageProcessor->getName(method), peformanceBenchmark, pyImageProcessor.get(), image, method);
+    }
+
+    ::benchmark::Initialize(&argc, argv);
+
+    if (::benchmark::ReportUnrecognizedArguments(argc, argv))
+        return 1;
+
+    ::benchmark::RunSpecifiedBenchmarks();
+}
