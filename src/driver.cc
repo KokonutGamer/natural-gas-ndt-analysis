5310e3a0e2ca3c6c9097718962f65a120c2ef515
Gabe Lapingcao <mgultra2018@gmail.com>
2026-06-04 21:49:19 -0700

    Updated default image file extension to bmp

--- src/driver.cc
+++ src/driver.cc
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
 
   options.add_options()("p,python", "Run with the embedded Python interpreter")("h,help", "Print usage")(
       "i,image", "Process an image",
-      cxxopts::value<std::string>()->default_value("./images/0070.tiff"))(
+      cxxopts::value<std::string>()->default_value("./images/0070.bmp"))(
       "o,output", "Output the processed image to a file",
       cxxopts::value<std::string>()->default_value("./processed/test_result.png"))(
       "a,algorithm", "Python algorithm used for processing the image",
